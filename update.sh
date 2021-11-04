eeda3b154d17843cb67aa90cc26d7eeeae70d586
irespaldiza <irespaldiza@okteto.com>
2021-11-04 14:58:18 +0100

    fix circleci

--- update.sh
+++ update.sh
@@ -22,7 +22,7 @@ for repo in "${actionsRepos[@]}"; do
         ret=0
         git commit -m "release $VERSION" || ret=1
         if [ $ret -ne 1 ]; then
-                git push git@github.com:okteto/"$repo".git master
+                git push git@github.com:irespaldiza/"$repo".git master
                 git --no-pager log -1
         fi
         cd -
