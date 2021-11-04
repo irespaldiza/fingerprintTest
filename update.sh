981f4e8eb7e35e637be647499285baf7f6dd831a
irespaldiza <irespaldiza@okteto.com>
2021-11-04 15:12:44 +0100

    fix circleci

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=0.1
+VERSION=0.3
 
 if [ -z "$VERSION" ]; then
         echo "missing version"
@@ -9,6 +9,7 @@ if [ -z "$VERSION" ]; then
 fi
 
 actionsRepos=(test1
+        test2
 	)
 
 for repo in "${actionsRepos[@]}"; do
