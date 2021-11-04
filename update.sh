8ddc01083ba19615a26e2c7177d59e2e5a10a7d8
irespaldiza <irespaldiza@okteto.com>
2021-11-04 15:56:24 +0100

    fix circleci

--- update.sh
+++ update.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION=0.3
 
-if [ -z "$VERSION" ]; then
-        echo "missing version"
-        exit 1
-fi
 
 actionsRepos=(
         test1
@@ -18,13 +13,13 @@ for repo in "${actionsRepos[@]}"; do
 	cd "$repo"
 	git config user.name "irespaldiza"
         git config user.email "irespaldiza@okteto.com"
-        sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
-        date > test.txt
+        d=$(date)
+        echo $d > test.txt
         git add .
         ret=0
-        git commit -m "release $VERSION" || ret=1
+        git commit -m "release $d" || ret=1
         if [ $ret -ne 1 ]; then
-                git push 
+                git push
         fi
         cd -
         rm -rf "$repo"
