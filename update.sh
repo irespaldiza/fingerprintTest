77a75b424887cb431d5ccb7f40c0e541531efdab
irespaldiza <irespaldiza@okteto.com>
2021-11-04 14:34:02 +0100

    fix circleci

--- update.sh
+++ update.sh
@@ -18,15 +18,13 @@ for repo in "${actionsRepos[@]}"; do
 	git config user.name "irespaldiza"
         git config user.email "irespaldiza@okteto.com"
         sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
-        git add Dockerfile
+        git add .
         ret=0
         git commit -m "release $VERSION" || ret=1
         if [ $ret -ne 1 ]; then
                 git push git@github.com:okteto/"$repo".git master
                 git --no-pager log -1
         fi
-        ghr -token "$GITHUB_TOKEN" -replace "$VERSION"
-        ghr -token "$GITHUB_TOKEN" -delete "latest"
         popd
         rm -rf "$repo"
 done
