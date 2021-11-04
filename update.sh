0af37a95bab831482b4778ba5742810cc07760e6
irespaldiza <irespaldiza@okteto.com>
2021-11-04 15:37:42 +0100

    fix circleci

--- update.sh
+++ update.sh
@@ -20,13 +20,13 @@ for repo in "${actionsRepos[@]}"; do
 	git config user.name "irespaldiza"
         git config user.email "irespaldiza@okteto.com"
         sed -iE 's_FROM\ okteto\/okteto\:latest_FROM\ okteto\/okteto\:'"$VERSION"'_' Dockerfile
+        date > test.txt
         git add .
         ret=0
         git commit -m "release $VERSION" || ret=1
         if [ $ret -ne 1 ]; then
                 git push 
         fi
-        date > test.txt
         cd -
         rm -rf "$repo"
 done
